a0b79ac2fe56e6edb2ccb5c9b201847e4c26d585
norton <norton.vj@outlook.com>
2024-04-19 16:50:00 -0300

    .

--- lista_ordenada.py
+++ lista_ordenada.py
@@ -1,14 +1,24 @@
-lista = [1,4,2,7,3]
+# Lista original e lista ordenada manualmente
+lista = [1, 4, 2, 7, 3]
 lista_ordenada = []
 
-for num in lista: #percorre os numeros dentro da lista
-    # if not lista_ordenada:
-    #     lista_ordenada.append(num) adiciona o primeiro numero assim iniciando o loop
-    for i in range(len(lista_ordenada)): # loop com range da lista ordenada
-        if num > lista_ordenada[i]:
-            lista_ordenada.insert(i,num)
+# Estudo de lista ordenada feita de forma "manual lidando com a lógica em posicionamento"
+for num in lista:  # Percorre os números dentro da lista
+    for i in range(len(lista_ordenada)):  # Loop com range da lista ordenada
+        if num > lista_ordenada[i]:  # Comparativo de número(lista) com número(lista_ordenada)
+            lista_ordenada.insert(i, num)  # Insere o número no índice no momento em que ele for maior que o número na lista ordenada
             break
     else:
-        lista_ordenada.append(num) #adiciona o primeiro numero assim iniciando o loop(pode-se usar um if para iniciar tambem)
+        lista_ordenada.append(num)  # Adiciona o primeiro número assim iniciando o loop
 
- 
+print("Lista ordenada manualmente:", lista_ordenada)
+
+# Lista ordenada usando o método sort()
+lista2 = [4, 2, 7, 3, 9]
+lista2.sort()  # .sort() modifica a lista de forma ordenada, podendo usar .sort(reverse=True)
+print("Lista ordenada com sort():", lista2)
+
+# Lista ordenada usando o método sorted()
+lista3 = [2, 10, 3, 1, 8]
+lista_ordenada3 = sorted(lista3)  # sorted() ordena a lista sem modificar a lista original, podendo usar sorted(lista3,reverse=True)
+print("Lista ordenada com sorted():", lista_ordenada3)
