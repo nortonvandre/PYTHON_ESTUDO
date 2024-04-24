cd3016cade90cc8beeb1bc468f9f61b2449216ee
norton <norton.vj@outlook.com>
2024-04-24 19:19:20 -0300

    .

--- busca_e_soma.py
+++ busca_e_soma.py
@@ -30,3 +30,45 @@ if resultado:
     print(f"Os números {resultado[0]} e {resultado[1]} têm a soma igual a {numero}.")
 else:
     print("Nenhum par de números encontrado.")
+
+
+
+
+def search(lista, numero):
+    """
+    Esta função procura dois números na lista cuja soma seja igual a um número dado.
+
+    Parâmetros:
+    - lista: lista de números inteiros
+    - numero: número inteiro para o qual a soma dos dois números deve ser igual
+
+    Retorna:
+    - Tupla com os dois números cuja soma é igual a 'numero', ou None se nenhum par for encontrado
+    """
+
+    # Conjunto para armazenar os números já vistos
+    numeros_vistos = set()
+
+    for n in lista:
+        complemento = numero - n
+
+        # Verifica se o complemento já foi visto
+        if complemento in numeros_vistos:
+            return n, complemento
+
+        # Adiciona o número atual ao conjunto de números vistos
+        numeros_vistos.add(n)
+
+    # Se nenhum par for encontrado, retorna None
+    return None
+
+# Lista de números e número alvo
+lista = [2, 6, 3, 9, 4]
+numero = 11
+
+# Chama a função e imprime os resultados
+resultado = search(lista, numero)
+if resultado:
+    print(f"Os números {resultado[0]} e {resultado[1]} têm a soma igual a {numero}.")
+else:
+    print("Nenhum par de números encontrado.")
